137d985c51d16ae27ca85750bde606207114d80e
TiagoAlb12 <t.albuquerque@ua.pt>
2024-04-15 17:40:13 +0100

    home page

--- App/app/src/main/java/com/example/app/MainActivity.kt
+++ App/app/src/main/java/com/example/app/MainActivity.kt
@@ -10,6 +10,14 @@ import com.example.app.map.MapFragment
 import com.example.app.qrcode.QrcodeFragment
 import com.example.app.ticket.TicketFragment
 import com.example.app.wallet.WalletFragment
+import android.app.DatePickerDialog
+import android.widget.DatePicker
+import android.view.View
+import java.util.*
+import android.widget.EditText
+import android.widget.Toast
+import java.text.SimpleDateFormat
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -61,4 +69,68 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    fun exibirDatePicker(view: View) {
+        val textbox = view as EditText
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            textbox.context,
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val currentDate = sdf.format(Date())
+                if (selectedDate >= currentDate &&
+                    (textbox.id == R.id.returnBox || selectedDate >= (findViewById<EditText>(R.id.departureBox).text.toString()))) {
+                    textbox.setText(selectedDate)
+                } else {
+                    Toast.makeText(textbox.context, "Escolha uma data válida", Toast.LENGTH_SHORT).show()
+                }
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis() - 1000
+        if (textbox.id == R.id.returnBox) { // Se o EditText for ReturnBox
+            val departureBox = findViewById<EditText>(R.id.departureBox)
+            // Definir a data mínima como a data selecionada na DepartureBox
+            val departureDate = departureBox.text.toString()
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val minDate = sdf.parse(departureDate).time
+            datePickerDialog.datePicker.minDate = minDate
+        }
+        datePickerDialog.show()
+    }
+
+    //para a caixa de texto do Return
+    fun onClickReturnBox(view: View) {
+        val returnBox = findViewById<EditText>(R.id.returnBox)
+        exibirDatePicker(returnBox)
+    }
+
+    //para a caixa de texto do Departure
+    fun onClickDepartureBox(view: View) {
+        val departureBox = findViewById<EditText>(R.id.departureBox)
+        exibirDatePicker(departureBox)
+    }
+
+    fun limparCaixasTexto(view: View) {
+        val fromBox = findViewById<EditText>(R.id.fromBox)
+        val toBox = findViewById<EditText>(R.id.toBox)
+        val departureBox = findViewById<EditText>(R.id.departureBox)
+        val returnBox = findViewById<EditText>(R.id.returnBox)
+
+        // Limpar o texto em todas as caixas de texto
+        fromBox.setText("")
+        toBox.setText("")
+        departureBox.setText("")
+        returnBox.setText("")
+    }
+
+
 }
