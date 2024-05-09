8a4f308476ff376028b6454b7b1f8675b08795f8
ZGamer59 <j.m.mendes@ua.pt>
2024-05-09 01:31:01 +0100

    more

--- App/app/src/main/java/com/example/app/buyTicket/BuyTicketFragment.kt
+++ App/app/src/main/java/com/example/app/buyTicket/BuyTicketFragment.kt
@@ -20,12 +20,6 @@ class BuyTicketFragment : Fragment() {
 
     private val viewModel: BuyTicketViewModel by viewModels()
 
-    fun onTicketButtonClick(view: View) {
-        // Aumenta o contador em 1
-        val mainActivity = activity as MainActivity
-        mainActivity.incrementCartItemCount()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,6 +33,20 @@ class BuyTicketFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_buy_ticket, container, false)
 
         val firstTicket = view.findViewById<ImageView>(R.id.first_ticket)
+        val secondTicket = view.findViewById<ImageView>(R.id.second_ticket)
+        val thirdTicket = view.findViewById<ImageView>(R.id.third_ticket)
+        val fourthTicket = view.findViewById<ImageView>(R.id.fourth_ticket)
+        val fifthTicket = view.findViewById<ImageView>(R.id.fifth_ticket)
+        val sixthTicket = view.findViewById<ImageView>(R.id.sixth_ticket)
+        val seventhTicket = view.findViewById<ImageView>(R.id.seventh_ticket)
+        val eighthTicket = view.findViewById<ImageView>(R.id.eighth_ticket)
+        val ninthTicket = view.findViewById<ImageView>(R.id.ninth_ticket)
+        val tenthTicket = view.findViewById<ImageView>(R.id.tenth_ticket)
+        val eleventhTicket = view.findViewById<ImageView>(R.id.eleventh_ticket)
+        val twelfthTicket = view.findViewById<ImageView>(R.id.twelfth_ticket)
+        val thirteenthTicket = view.findViewById<ImageView>(R.id.thirteenth_ticket)
+        val fourteenthTicket = view.findViewById<ImageView>(R.id.fourteenth_ticket)
+
         firstTicket.setOnClickListener {
             if (!isButtonClicked) {
                 // Aumenta o contador em 1
@@ -46,6 +54,344 @@ class BuyTicketFragment : Fragment() {
                 mainActivity.incrementCartItemCount()
                 // Desativa o clique no botão
                 firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        secondTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        thirdTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        fourthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        fifthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        sixthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        seventhTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        eighthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        ninthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        tenthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        eleventhTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        twelfthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        thirteenthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
+                // Define o estado do botão como clicado
+                isButtonClicked = true
+            }
+        }
+
+        fourteenthTicket.setOnClickListener {
+            if (!isButtonClicked) {
+                // Aumenta o contador em 1
+                val mainActivity = activity as MainActivity
+                mainActivity.incrementCartItemCount()
+                // Desativa o clique no botão
+                firstTicket.isClickable = false
+                secondTicket.isClickable = false
+                thirdTicket.isClickable = false
+                fourthTicket.isClickable = false
+                fifthTicket.isClickable = false
+                sixthTicket.isClickable = false
+                seventhTicket.isClickable = false
+                eighthTicket.isClickable = false
+                ninthTicket.isClickable = false
+                tenthTicket.isClickable = false
+                eleventhTicket.isClickable = false
+                twelfthTicket.isClickable = false
+                thirteenthTicket.isClickable = false
+                fourteenthTicket.isClickable = false
                 // Define o estado do botão como clicado
                 isButtonClicked = true
             }
